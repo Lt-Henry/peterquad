285116b405fe8a084e933c6a35d8304f9e3b033a
Lt-Henry <quiqueiii@gmail.com>
2015-06-15 14:48:20 +0200

    just a pair of lines

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -8,12 +8,13 @@ using namespace std;
 
 int main(int argc,char * argv[])
 {
-	int socket_desc;
-	struct sockaddr_in server;
+	int socket_fd, client_fd;
+	int cli_len;
+	struct sockaddr_in server, client;
 	
-	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+	socket_fd = socket(AF_INET , SOCK_STREAM , 0);
 	
-	if(socket_desc==-1)throw runtime_error("Could not create socket");
+	if(socket_fd==-1)throw runtime_error("Could not create socket");
 	
 	cout<<"Socket created"<<endl;
 	
@@ -21,14 +22,23 @@ int main(int argc,char * argv[])
 	server.sin_addr.s_addr = INADDR_ANY;
 	server.sin_port = htons(7070);
 	
-	if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+	if( bind(socket_fd,(struct sockaddr *)&server , sizeof(server)) < 0)
 		throw runtime_error("Bind failed");
 		
 	cout<<"Bind done"<<endl;
 	
-	listen(socket_desc , 1);
+	listen(socket_fd , 1);
 	
 	cout<<"Wait for client..."<<endl;
+	
+	cli_len=sizeof(client);
+	
+	client_fd=accept(socket_fd,(struct sockaddr *) &client,&cli_len);
+	
+	if(client_fd<0)
+	{
+		throw runtime_error("Accept failed");
+	}
 	
 	return 0;
 }
